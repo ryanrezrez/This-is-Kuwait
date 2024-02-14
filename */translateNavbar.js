0cd5363b63241fffb8010b927cd6d558b2fbafe3
Ryan Rezaei <ryanrez5511@gmail.com>
2024-02-14 17:04:14 +0300

    Speed Fix 2

--- */translateNavbar.js
+++ */translateNavbar.js
@@ -45,6 +45,21 @@ function updateContent() {
     }
 }
 
+function switchMainLanguage(page) {
+    switchLanguage();
+    if (page === 'home') {
+        switchLanguageHome();
+    } else if (page === 'learnkuwaiti') {
+        switchLanguageLK();
+    } else if (page === 'gallery') {
+        switchLanguageGallery();
+    } else if (page === 'aboutus') {
+        switchLanguageAboutUs();
+    } else if (page === 'sponsors') {
+        switchLanguageSponsors();
+    }
+}
+
 function switchLanguage() {
     currentLanguage = currentLanguage === 'en' ? 'ar' : 'en';
     localStorage.setItem('language', currentLanguage);
