9d3f858e8fd815f12fb0ba77c7ab3951f1abca86
Ryan Rezaei <ryanrez5511@gmail.com>
2024-04-17 15:27:36 +0300

    bug fix

--- */translateNavbar.js
+++ */translateNavbar.js
@@ -1,1 +1,69 @@
-let currentLanguage=localStorage.getItem("language")||"en";let translations={};fetch("../*/translations/navbar.json").then(response=>response.json()).then(data=>{translations=data;updateContent()}).catch(error=>console.error("Error loading translations:",error));function updateContent(){const elements=document.querySelectorAll(".link");elements.forEach(element=>{const classes=element.classList;if(classes.contains("home")){element.textContent=translations[currentLanguage].home}else if(classes.contains("learnkuwaiti")){element.textContent=translations[currentLanguage].learnkuwaiti}else if(classes.contains("gallery")){element.textContent=translations[currentLanguage].gallery}else if(classes.contains("aboutus")){element.textContent=translations[currentLanguage].aboutus}else if(classes.contains("sponsors")){element.textContent=translations[currentLanguage].sponsors}else if(classes.contains("switchLanguage")){element.textContent=translations[currentLanguage].switchlang}});const desktopContainer=document.querySelectorAll(".desktop-container");if(currentLanguage==="en"){desktopContainer.forEach(desktopContainer=>{desktopContainer.style.direction="ltr"})}else{desktopContainer.forEach(desktopContainer=>{desktopContainer.style.direction="rtl"})}}function switchMainLanguage(page){switchLanguage();if(page==="home"){switchLanguageHome()}else if(page==="learnkuwaiti"){switchLanguageLK()}else if(page==="gallery"){switchLanguageGallery()}else if(page==="aboutus"){switchLanguageAboutUs()}else if(page==="sponsors"){switchLanguageSponsors()}}function switchLanguage(){currentLanguage=currentLanguage==="en"?"ar":"en";localStorage.setItem("language",currentLanguage);updateContent()}
+let currentLanguage = localStorage.getItem("language") || "en";
+let translations = {};
+
+fetch("../*/translations/navbar.json")
+    .then(response => response.json())
+    .then(data => {
+        translations = data;
+        updateContent();
+    })
+    .catch(error => console.error("Error loading translations:", error));
+
+function updateContent() {
+    const elements = document.querySelectorAll(".link");
+    
+    elements.forEach(element => {
+        const classes = element.classList;
+        
+        if (classes.contains("home")) {
+            element.textContent = translations[currentLanguage].home;
+        } else if (classes.contains("learnkuwaiti")) {
+            element.textContent = translations[currentLanguage].learnkuwaiti;
+        } else if (classes.contains("gallery")) {
+            element.textContent = translations[currentLanguage].gallery;
+        } else if (classes.contains("aboutus")) {
+            element.textContent = translations[currentLanguage].aboutus;
+        } else if (classes.contains("sponsors")) {
+            element.textContent = translations[currentLanguage].sponsors;
+        } else if (classes.contains("switchLanguage")) {
+            element.textContent = translations[currentLanguage].switchlang;
+        }
+    });
+
+    const desktopContainer = document.querySelectorAll(".desktop-container");
+    
+    if (currentLanguage === "en") {
+        desktopContainer.forEach(desktopContainer => {
+            desktopContainer.style.direction = "ltr";
+        });
+    } else {
+        desktopContainer.forEach(desktopContainer => {
+            desktopContainer.style.direction = "rtl";
+        });
+    }
+
+    const title = document.getElementById('title'); 
+    title.addEventListener('click', () => { window.location.href = '../index.html'; });
+}
+
+function switchMainLanguage(page) {
+    switchLanguage();
+
+    if (page === "home") {
+        switchLanguageHome();
+    } else if (page === "learnkuwaiti") {
+        switchLanguageLK();
+    } else if (page === "gallery") {
+        switchLanguageGallery();
+    } else if (page === "aboutus") {
+        switchLanguageAboutUs();
+    } else if (page === "sponsors") {
+        switchLanguageSponsors();
+    }
+}
+
+function switchLanguage() {
+    currentLanguage = currentLanguage === "en" ? "ar" : "en";
+    localStorage.setItem("language", currentLanguage);
+    updateContent();
+}
