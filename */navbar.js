ea7a7149c60ae8d629ad88a8b6f50cbd839dbef1
Ryan Rezaei <ryanrez5511@gmail.com>
2024-04-17 15:21:27 +0300

    home title button

--- */navbar.js
+++ */navbar.js
@@ -1,1 +1,71 @@
-var mobileMenuBtn=document.querySelector("#mobile-menu-btn");var mobileContainer=document.querySelector(".mobile-container");mobileMenuBtn.addEventListener("click",()=>{if(mobileContainer.style.display==="flex"){mobileContainer.style.display="none"}else{mobileContainer.style.display="flex"}});
+let currentLanguage = localStorage.getItem("language") || "en";
+let translations = {};
+
+fetch("../*/translations/navbar.json")
+    .then(response => response.json())
+    .then(data => {
+        translations = data;
+        updateContent();
+    })
+    .catch(error => console.error("Error loading translations:", error));
+
+function updateContent() {
+    const elements = document.querySelectorAll(".link");
+    
+    elements.forEach(element => {
+        const classes = element.classList;
+        
+        if (classes.contains("home")) {
+            element.textContent = translations[currentLanguage].home;
+        } else if (classes.contains("learnkuwaiti")) {
+            element.textContent = translations[currentLanguage].learnkuwaiti;
+        } else if (classes.contains("gallery")) {
+            element.textContent = translations[currentLanguage].gallery;
+        } else if (classes.contains("aboutus")) {
+            element.textContent = translations[currentLanguage].aboutus;
+        } else if (classes.contains("sponsors")) {
+            element.textContent = translations[currentLanguage].sponsors;
+        } else if (classes.contains("switchLanguage")) {
+            element.textContent = translations[currentLanguage].switchlang;
+        }
+    });
+
+    const desktopContainer = document.querySelectorAll(".desktop-container");
+    
+    if (currentLanguage === "en") {
+        desktopContainer.forEach(desktopContainer => {
+            desktopContainer.style.direction = "ltr";
+        });
+    } else {
+        desktopContainer.forEach(desktopContainer => {
+            desktopContainer.style.direction = "rtl";
+        });
+    }
+
+    const title = document.getElementById('title');
+    title.addEventListener('click', () => {
+        window.location.href = '../index.html';
+    });
+}
+
+function switchMainLanguage(page) {
+    switchLanguage();
+
+    if (page === "home") {
+        switchLanguageHome();
+    } else if (page === "learnkuwaiti") {
+        switchLanguageLK();
+    } else if (page === "gallery") {
+        switchLanguageGallery();
+    } else if (page === "aboutus") {
+        switchLanguageAboutUs();
+    } else if (page === "sponsors") {
+        switchLanguageSponsors();
+    }
+}
+
+function switchLanguage() {
+    currentLanguage = currentLanguage === "en" ? "ar" : "en";
+    localStorage.setItem("language", currentLanguage);
+    updateContent();
+}
