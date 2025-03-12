cd2d52f0c3fc93ced19ca7531ac78cb805bc5953
ryanrez <ryanrez@penguin.lxd>
2025-03-12 16:06:36 +0300

    New early update

--- */navbar.js
+++ */navbar.js
@@ -1,1 +1,14 @@
+<<<<<<< HEAD
 var mobileMenuBtn=document.querySelector("#mobile-menu-btn");var mobileContainer=document.querySelector(".mobile-container");mobileMenuBtn.addEventListener("click",()=>{if(mobileContainer.style.display==="flex"){mobileContainer.style.display="none"}else{mobileContainer.style.display="flex"}});
+=======
+const mobileMenuBtn = document.querySelector("#mobile-menu-btn");
+const mobileContainer = document.querySelector(".mobile-container");
+
+mobileMenuBtn.addEventListener("click", () => {
+    if (mobileContainer.style.display === "flex") {
+        mobileContainer.style.display = "none";
+    } else {
+        mobileContainer.style.display = "flex";
+    }
+});
+>>>>>>> 6aefc1e (New early update)
